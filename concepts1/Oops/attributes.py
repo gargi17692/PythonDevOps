797b7c3ba6f48b5d3b67242571f7c9f8fb4a689e
gargi17692 <160726145+gargi17692@users.noreply.github.com>
2025-01-21 12:11:03 +0000

    updatedOnTue Jan 21 12:10:50 UTC 2025

--- concepts1/Oops/attributes.py
+++ concepts1/Oops/attributes.py
@@ -1,9 +1,16 @@
 # Simple python scripts to maintain employee data
 class Employee:
+    # If you define a variable immediately after declaration of class(class related variable); class attribute.
+    count=0
     def get_name_age_salary(self,name,age,salary):
         self.name=name
         self.age=age
         self.salary=salary
+        self.increase_employee_count()
+        self.display_details()
+        return None
+    def increase_employee_count(self):
+        Employee.count=Employee.count+1
         return None
     def display_details(self):
         print(f'The name is : {self.name}')
@@ -15,6 +22,9 @@ employee1=Employee()
 employee2=Employee()
 
 employee1.get_name_age_salary('Radha',16,'$30k')
-employee1.display_details()
+# employee1.display_details()
+# employee1.increase_employee_count()
 employee2.get_name_age_salary('Krishna',12,'$16k')
-employee2.display_details()
+# employee2.display_details()
+# employee2.increase_employee_count()
+# print(Employee.count)
