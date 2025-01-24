d3d9b85845b5d3baabf0734442498e2bfc6a4114
gargi17692 <160726145+gargi17692@users.noreply.github.com>
2025-01-24 14:20:08 +0000

    updtedOnFri Jan 24 14:19:52 UTC 2025

--- practice1/OOP_Practice/structued_Format/item.py
+++ practice1/OOP_Practice/structued_Format/item.py
@@ -7,7 +7,7 @@ class Item(object):
          assert price >= 0, f"price {price} is not greater or equal to zero!"
          assert quantity >= 0, f"quantity {quantity} is not greater or equal to zero!"
          # Assign to self object
-         self._name=name
+         self.__name=name # Making name private to the class once it's instantiated 
          self.price=price
          self.quantity=quantity
          # Actions to execute
@@ -16,7 +16,14 @@ class Item(object):
       @property
       # Property Decorator = Read-Only Atribute
       def name(self):
-          return self._name      
+          #print("you are tring to get")
+          return self.__name      
+       
+      @name.setter
+      # Yet we want to set new value for *name*
+      def name(self,value):
+           #print("Ypu are trying to set")
+           self.__name=value
 
       def apply_dicount(self):
            self.price=self.price*self.pay_rate
--- practice1/OOP_Practice/structued_Format/main.py
+++ practice1/OOP_Practice/structued_Format/main.py
@@ -3,4 +3,9 @@ from item import Item
 
 item1=Item("MyItem", 750)
 #item1.name="OtherItem" # AttributeError: property 'name' of 'Item' object has no setter
+#print(item1.__name) # AttributeError: 'Item' object has no attribute '__name'.
+
+# Setting an Attribute
+item1.name="OtherItem"
+# Getting an Attribute
 print(item1.name)
