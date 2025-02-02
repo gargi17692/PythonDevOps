f1f1727966facd56f5f43678f3b2d78a66687887
gargi17692 <160726145+gargi17692@users.noreply.github.com>
2025-02-02 20:47:58 +0000

    updatedOnSun Feb  2 20:47:44 UTC 2025

--- Flask_Market/market.py
+++ Flask_Market/market.py
@@ -5,4 +5,13 @@ app=Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+     return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+      items = [
+      {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 50000},
+      {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 90000},
+      {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 1500}
+      ]
+      return render_template('market.html', items=items)
